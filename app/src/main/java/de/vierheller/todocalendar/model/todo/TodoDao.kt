b53032d60216e8409dda490d0c17ae0b76f4586d
Vierheller <j.vierheller@outlook.de>
2017-11-21 22:07:55 +0100

    Added Anki library and fixed crashes

--- app/src/main/java/de/vierheller/todocalendar/model/todo/TodoDao.kt
+++ app/src/main/java/de/vierheller/todocalendar/model/todo/TodoDao.kt
@@ -3,6 +3,7 @@ package de.vierheller.todocalendar.model.todo
 import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 
 /**
@@ -13,6 +14,6 @@ interface TodoDao {
     @Query("SELECT * FROM tasks")
     fun getAllTodos(): LiveData<List<Task>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(todoEntity: Task)
 }
--- app/src/main/java/de/vierheller/todocalendar/repository/TodoRepository.kt
+++ app/src/main/java/de/vierheller/todocalendar/repository/TodoRepository.kt
@@ -3,6 +3,7 @@ package de.vierheller.todocalendar.repository
 import android.arch.lifecycle.LiveData
 import de.vierheller.todocalendar.TodoCalendarApplication
 import de.vierheller.todocalendar.model.todo.Task
+import org.jetbrains.anko.doAsync
 
 /**
  * Created by Vierheller on 01.11.2017.
@@ -14,7 +15,9 @@ class TodoRepository(){
     }
 
     fun putTodo(entity: Task) {
-        TodoCalendarApplication.database.TodoDao().insert(entity)
+        doAsync {
+            TodoCalendarApplication.database.TodoDao().insert(entity)
+        }
     }
 
 
