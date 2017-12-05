5b79044b66bccb8e374481e82da278ee49f1895d
Jan-Niklas Vierheller <j.vierheller@outlook.de>
2017-12-05 09:35:24 +0100

    Added comment to extension function

--- app/src/main/java/de/vierheller/todocalendar/extensions/ActivityExtensions.kt
+++ app/src/main/java/de/vierheller/todocalendar/extensions/ActivityExtensions.kt
@@ -7,8 +7,11 @@ import android.content.res.TypedArray
  * Created by Vierheller on 05.12.2017.
  */
 
-fun Activity.getListFromResourceArray(resourceArray: TypedArray): MutableList<Int> {
-    val list = MutableList<Int>(resourceArray.length()) { i ->
+/**
+ * This function returns an Array of ResourceIds based on the resourceArray provided
+ */
+fun Activity.getListFromResourceArray(resourceArray: TypedArray): List<Int> {
+    val list = List<Int>(resourceArray.length()) { i ->
         resourceArray.getResourceId(i, -1)
     }
     resourceArray.recycle()
