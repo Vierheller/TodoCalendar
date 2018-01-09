86e9fa28e10bc250a9bbfa68f30e1b3671bf0a62
Jan-Niklas Vierheller <j.vierheller@outlook.de>
2018-01-09 18:50:23 +0100

    Fixed Layout Error

--- app/src/main/java/de/vierheller/todocalendar/view/main/DayViewFragment.kt
+++ app/src/main/java/de/vierheller/todocalendar/view/main/DayViewFragment.kt
@@ -34,7 +34,6 @@ class DayViewFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-
         return inflater!!.inflate(R.layout.fragment_day_view, container, false)
     }
 
